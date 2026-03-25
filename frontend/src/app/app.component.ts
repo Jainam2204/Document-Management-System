2dea4eb5e45b534d402aeeafb3ec49276746c923
Jainam2204 <jainamvora2201@gmail.com>
2026-03-25 19:58:37 +0530

    added shared header and sidebar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './shared/components/header/header.component';
+import { SidebarComponent } from './shared/components/sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, SidebarComponent, FormsModule, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'frontend';
+  constructor(private router: Router) { }
+
+  isAuthPage(): boolean {
+    return this.router.url.includes('login') ||
+      this.router.url.includes('verify') ||
+      this.router.url.includes('register');
+  }
 }
